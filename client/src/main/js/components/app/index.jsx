fe4ec554cb707f03bba296bf6bce7b2dcf9f69f6
mszalbach <4-bar-killa@web.de>
2016-09-12 16:30:52 +0200

    - used react-refetch to get data from the rest server

--- client/src/main/js/components/app/index.jsx
+++ client/src/main/js/components/app/index.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { Provider } from 'react-redux';
 import GreetingList from "components/greetingList";
-import { createStore } from 'redux'
-import greetings from "../../reducers";
 
 export default class App extends React.Component {
 
     render() {
-        const store = createStore(greetings);
-
-        return <Provider store={store}>
-                    <GreetingList greetings={store.getState()} />
-                </Provider>;
+        return <GreetingList  />;
     }
 }
--- client/src/main/js/components/greetingList/index.jsx
+++ client/src/main/js/components/greetingList/index.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { Component } from 'react'
+import { connect, PromiseState } from 'react-refetch'
 
+class GreetingList extends Component {
+    render() {
+        const { fetch } = this.props
 
-export default ( {greetings} ) => (
+        if (fetch.pending) {
+            return <div>Loading</div>
+        } else if (fetch.rejected) {
+            return <div>ERROR: {userFetch.reason}</div>
+        } else if (fetch.fulfilled) {
+            return <ul>
+                {fetch.value.map( greeting =>
+                                              <li key={greeting.id}>{greeting.content}</li>
 
-<ul>
-    {greetings.map( greeting =>
-                            <li key={greeting.id}>{greeting.content}</li>
+                )}
 
-    )}
+            </ul>
+        }
 
-</ul>
-);
+        // similar for `likesFetch`
+    }
+}
+
+export default connect(props => ({
+    fetch: `http://localhost:8081/greetings`
+}))(GreetingList)
